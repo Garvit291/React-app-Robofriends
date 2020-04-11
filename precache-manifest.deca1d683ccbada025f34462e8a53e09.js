681d1e3976132df2087ec0bf9f7c665ddf818a83
Garvit291 <chhabragarvit291@outlook.com>
2020-04-11 18:05:09 +0530

    Updates

---
+++ precache-manifest.deca1d683ccbada025f34462e8a53e09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d7b133f2a8452aee597ee56e6a9df6",
+    "url": "/React-app-Robofriends/index.html"
+  },
+  {
+    "revision": "c63e519da06ad4c6eee3",
+    "url": "/React-app-Robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8c1b60c4cab1a42b2d44",
+    "url": "/React-app-Robofriends/static/css/main.4b509d3f.chunk.css"
+  },
+  {
+    "revision": "c63e519da06ad4c6eee3",
+    "url": "/React-app-Robofriends/static/js/2.24419c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-Robofriends/static/js/2.24419c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1b60c4cab1a42b2d44",
+    "url": "/React-app-Robofriends/static/js/main.c3245e70.chunk.js"
+  },
+  {
+    "revision": "cb56e20bc997eacf032b",
+    "url": "/React-app-Robofriends/static/js/runtime-main.098889df.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-app-Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
